624deaefc9be68b003c0025c269b609802c1c199
Sebastian Krajewski <mail@wieku.me>
2020-06-17 19:32:15 +0200

    Initial commit

---
+++ src/main/java/pl/edu/agh/student/dejakraj/tvprogramme/Logging.java
@@ -0,0 +1,66 @@
+package pl.edu.agh.student.dejakraj.tvprogramme;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.builder.api.*;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
+
+import java.util.ArrayList;
+
+public class Logging {
+    private static final String consolePattern = "%d{yyyy-MM-dd' 'HH:mm:ss.SSS} [%t] [%c] [%p]: %m%n";
+    private static final String filePattern = "%d{yyyy-MM-dd' 'HH:mm:ss.SSS} [%t] [%p]: %m%n";
+    private static final String logRoot = "./logs/";
+
+    private static final ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
+
+    private static final LayoutComponentBuilder consoleLayout;
+    private static final LayoutComponentBuilder fileLayout;
+
+    private static final ArrayList<String> loggers = new ArrayList<>();
+
+    static {
+        consoleLayout = builder.newLayout("PatternLayout");
+        consoleLayout.addAttribute("pattern", consolePattern);
+
+        fileLayout = builder.newLayout("PatternLayout");
+        fileLayout.addAttribute("pattern", filePattern);
+
+
+        AppenderComponentBuilder consoleAppender = builder.newAppender("stdout", "Console");
+        consoleAppender.add(consoleLayout);
+        builder.add(consoleAppender);
+    }
+
+    public static Logger getLogger(final String name) {
+        String lowerName = name.toLowerCase();
+        if (!loggers.contains(lowerName)) {
+            createLogger(lowerName);
+            loggers.add(lowerName);
+        }
+
+        return LogManager.getLogger(lowerName);
+    }
+
+    private static void createLogger(final String name) {
+        String lowerName = name.toLowerCase();
+
+        AppenderComponentBuilder file = builder.newAppender(lowerName, "File");
+        file.addAttribute("fileName", logRoot + lowerName + ".log");
+        file.addAttribute("append", "false");
+        file.add(fileLayout);
+
+        builder.add(file);
+
+        LoggerComponentBuilder logger = builder.newLogger(lowerName, Level.ALL);
+        logger.add(builder.newAppenderRef("stdout"));
+        logger.add(builder.newAppenderRef(lowerName));
+        logger.addAttribute("additivity", false);
+
+        builder.add(logger);
+
+        Configurator.reconfigure(builder.build());
+    }
+}
---
+++ src/main/java/pl/edu/agh/student/dejakraj/tvprogramme/Main.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.student.dejakraj.tvprogramme;
+
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = Logging.getLogger("TVProgramme");
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+    }
+
+}
