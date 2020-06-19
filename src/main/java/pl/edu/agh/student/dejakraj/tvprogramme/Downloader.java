5efa773479838f2e098f9f9378559f72d326b629
andrzejdeja <andrzejdeja@protonmail.com>
2020-06-19 07:25:15 +0200

    Add logs in Downloader and JsonDownloader

--- src/main/java/pl/edu/agh/student/dejakraj/tvprogramme/Downloader.java
+++ src/main/java/pl/edu/agh/student/dejakraj/tvprogramme/Downloader.java
@@ -19,14 +19,15 @@ public class Downloader {
     {
         LinkedHashMap<String, ArrayList<Program>> map = new LinkedHashMap<String, ArrayList<Program>>();
         try {
-            Document doc = Jsoup.connect("https://tv.mail.ru/moskva/").get();
-            Elements channels = doc.select("div.p-channels__item"); //get channels
+            Document doc = Jsoup.connect("https://tv.mail.ru/moskva/").get(); //Connect and get HTML Document
+            logger.info("Received HTML file from https://tv.mail.ru/moskva/");
+            Elements channels = doc.select("div.p-channels__item"); //Get channels
             channels.forEach((Element e) -> {
-                String channelName = e.select("a.p-channels__item__info__title__link").first().ownText(); //get channel names
+                String channelName = e.select("a.p-channels__item__info__title__link").first().ownText(); //Get channel names
                 ArrayList<Program> channelPrograms = new ArrayList<Program>();
-                e.select("div.p-programms__item__inner").forEach((Element program) -> { //get programs
+                e.select("div.p-programms__item__inner").forEach((Element program) -> { //Get programs
                     channelPrograms.add(new Program(program.select("span.p-programms__item__name-link").first().ownText(), //get program names
-                            program.select("span.p-programms__item__time-value").first().ownText())); //get program hour
+                            program.select("span.p-programms__item__time-value").first().ownText())); //Get program hour
                 });
                 if(!channelName.isEmpty()) { //save if not empty
                     map.put(channelName, channelPrograms);
@@ -37,6 +38,7 @@ public class Downloader {
             logger.error(e);
             return null;
         }
+        logger.info("Data parsed successfully");
         return map;
     }
 
--- src/main/java/pl/edu/agh/student/dejakraj/tvprogramme/JsonDownloader.java
+++ src/main/java/pl/edu/agh/student/dejakraj/tvprogramme/JsonDownloader.java
@@ -28,25 +28,27 @@ public class JsonDownloader {
         LinkedHashMap<String, ArrayList<Program>> map = new LinkedHashMap<String, ArrayList<Program>>();
         String url = "https://tv.mail.ru/ajax/index/?date=" + date; //rrrr-mm-dd format
         try{
-            BufferedReader rd = new BufferedReader(new InputStreamReader(new URL(url).openStream(), Charset.forName("UTF-8")));
+            BufferedReader rd = new BufferedReader(new InputStreamReader(new URL(url).openStream(), Charset.forName("UTF-8"))); //Connection
             String data = readAll(rd);
+            logger.info("Fetched data from: " + url);
             Gson gson = new Gson();
-            JsonObj obj = gson.fromJson(data, JsonObj.class);
+            JsonObj obj = gson.fromJson(data, JsonObj.class); //Moving data from JSON structure to Java object.
             for (int i = 0; i < obj.schedule.size(); i++) {
                 ArrayList<Program> pr = new ArrayList<Program>();
                 for (int j = 0; j < obj.schedule.get(i).event.size(); j++) {
                     String name = obj.schedule.get(i).event.get(j).name;
-                    if (!obj.schedule.get(i).event.get(j).episode_title.isEmpty())
-                        name = new String(name + " (" + obj.schedule.get(i).event.get(j).episode_title + ")");
+                    if (!obj.schedule.get(i).event.get(j).episode_title.isEmpty()) //Add episode name if exists
+                        name = name + " (" + obj.schedule.get(i).event.get(j).episode_title + ")";
                     pr.add(new Program(name, obj.schedule.get(i).event.get(j).start));
                 }
-                map.put(obj.schedule.get(i).channel.name, pr);
+                map.put(obj.schedule.get(i).channel.name, pr); //Saving each channel
             }
         }
         catch(Exception e) {
             logger.error(e);
             return null;
         }
+        logger.info("Data parsed successfully");
         return map;
     }
 
